4d4436227825e2cb13e72089f416fb75283c4e2b
bidzovska1234567 <43668868+bidzovska1234567@users.noreply.github.com>
2019-06-12 22:06:20 +0200

    cas1
    Prv Chas

---
+++ c1/index.js
@@ -0,0 +1,21 @@
+console.log('Helloz!');
+
+ime = 'Bojan';
+var prezime = 'Bojanovski';
+let vozrast = 36;
+
+console.log(ime);
+
+function test() {
+   // console.log(ime);
+   // console.log(prezime);
+  //  console.log(vozrast);
+    a = 10;
+    //var b = 20;
+    let c = 30;
+}
+
+test();
+console.log(a);
+//console.log(b);
+console.log(c);
