40249a3f8f659878ac14840d68889811fcd5a31e
bidzovska1234567 <43668868+bidzovska1234567@users.noreply.github.com>
2019-06-17 22:04:09 +0200

    Update v1.js

--- c2/v1.js
+++ c2/v1.js
@@ -18,23 +18,34 @@
 // }
 
 // everyThird(4);
-    function arrSum(a) {
-        var c = 0;
-        for(var i = 0; i < a.length; i++){
-            if(a[i].toString().length > 1) {
-                c++;
-                a[i] = a[i].toString().split('').reduce((a, b) => parseInt(a) = parseInt(b));
-            }
+    // function arrSum(a) {
+    //     var c = 0;
+    //     for(var i = 0; i < a.length; i++){
+    //         if(a[i].toString().length > 1) {
+    //             c++;
+    //             a[i] = a[i].toString().split('').reduce((a, b) => parseInt(a) = parseInt(b));
+    //         }
            
 
-        }
-    }
-    if(c > 0){
-        return arrSum(a);
-        } else {
-        return a.reduce((a. b) => a + b);
-        }
+    //     }
+    // }
+    // if(c > 0){
+    //     return arrSum(a);
+    //     } else {
+    //     return a.reduce((a. b) => a + b);
+    //     }
 
-    }
-    var r = arrSum([248, 21, 95, 239, 46]);
+    // }
+    // var r = arrSum([248, 21, 95, 239, 46]);
+    // console.log(r);
+
+    // module.exports = {
+    //     arrSum,
+    //     everyThird
+    // };
+    var func = require('./v2');
+
+    func.everyThird(2);
+
+    var r = func.arrSum([248, 21, 95, 239, 46]);
     console.log(r);
